527b75acd836aa7b0b0eae3864583625ad245695
Abhijeet Shrivastav <160402433+Abhijeet312@users.noreply.github.com>
2026-02-01 10:25:36 +0530

    Add files via upload

---
+++ JAVA/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello Bro");
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter Your Creativity :");
+        String creati = inp.nextLine();
+
+        System.out.println("Your Creativity is : " + creati);
+    }
+}
